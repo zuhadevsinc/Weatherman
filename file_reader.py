254fc7f6a0f08b24ec6e1053bc4b5559f17410b8
Zuha Sohail <zuha.ahmad@devsinc.com>
2025-08-27 17:15:40 +0500

    file-reader ftn

---
+++ file_reader.py
@@ -0,0 +1,46 @@
+import os
+
+
+def read_all_weather_files(folder_path):
+    all_weather_records = []
+    files = os.listdir(folder_path)
+
+    for filename in files:
+        if filename.endswith(".txt"):
+            file_path = os.path.join(folder_path, filename)
+            # print(f"Reading file: {file_path}")
+
+            with open(file_path, "r") as file:
+                lines = file.readlines()
+
+                for line in lines[1:]:
+                    parts = line.split(",")
+                    try:
+                        date = parts[0]  # "2006-6-1"
+                        date_parts = date.split("-")
+                        year = int(date_parts[0])
+                        month = int(date_parts[1])
+                        day = int(date_parts[2])
+
+                        max_temp = parts[1]
+                        min_temp = parts[3]
+                        max_humidity = parts[7]
+
+                        # print(date, max_temp, min_temp, max_humidity)
+
+                        # Creating a weather record (dictionary)
+                        weather_record = {
+                            "year": year,
+                            "month": month,
+                            "day": day,
+                            "date": date,
+                            "max_temp": max_temp,
+                            "min_temp": min_temp,
+                            "humidity": max_humidity,
+                        }
+
+                        all_weather_records.append(weather_record)
+
+                    except:
+                        continue
+    return all_weather_records
--- task.py
+++ task.py
@@ -5,7 +5,16 @@ from file_reader import read_all_weather_files
 
 def main():
 
-    print("Weatherman problem...")
+    # python3 task.py -e 2006 ./Murree_weather
+
+    option = sys.argv[1]  # -e, -a, or -c
+    date_input = sys.argv[2]  # year or year/month
+    folder_path = sys.argv[3]  # path to files
+
+    print("Reading weather files...")
+    all_records = read_all_weather_files(folder_path)
+    print(f"Found {len(all_records)} weather records")
+    print()
 
 
 if __name__ == "__main__":
